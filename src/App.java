76f44238ce2c15a441c9cd79566dbec63c20fa5c
quirogav20 <vic28q@email.com>
2025-11-12 22:35:06 -0600

    Dastos a ingresar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner lector = new Scanner(System.in);
+
+
+        System.out.println("Ingresa el valor de la demanda (D): ");
+        int d = lector.nextInt();
+        System.out.println("Ingresa el valor del costo de almacenamiento (h): ");
+        int h = lector.nextInt();
+        System.out.println("Ingresa el valor del costo de pedido (k): ");
+        int k = lector.nextInt();
+
+
+        double y = Math.sqrt((2 * d * k) / h);
+        System.out.println("La cantidad del pedido es: " + y);
+
+
     }
 }
