7b2f63b6e8f05988333dd7228d9dea95988a6c76
quirogav20 <vic28q@email.com>
2025-11-12 23:05:36 -0600

    Pruebas

--- src/App.java
+++ src/App.java
@@ -11,11 +11,22 @@ public class App {
         int h = lector.nextInt();
         System.out.println("Ingresa el valor del costo de pedido (k): ");
         int k = lector.nextInt();
+        System.out.println("Ingresa el tiempo que trada en llegar el pedido (L): ");
+        int l = lector.nextInt();
+        System.out.println("----------------------------------");
+        System.out.println("");
 
 
         double y = Math.sqrt((2 * d * k) / h);
         System.out.println("La cantidad del pedido es: " + y);
-
+        double t = (double) y / d;
+        System.out.println("El numero se realiza cada : " + t+ "dias");
+        double n = l/t;
+        double le = l-(Math.floor(n)*t);
+        System.out.println("El punto de reorden es: " + le*d);
+        double tcu = (k/(y / d) ) + (y / 2) * h;
+        System.out.println("El costo total del inventarios es: " + tcu);
+        System.out.println("----------------------------------");
 
     }
 }
